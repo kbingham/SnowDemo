befe5fc2b722b3e77d83d7b6a3a2bfc71e359910
Kieran Bingham <kieran@bingham.xyz>
2017-11-12 23:13:08 +0000

    SnowDemo: Clarify licensing as GPLv3

--- _8x32SmallHat.ino
+++ _8x32SmallHat.ino
@@ -1,3 +1,20 @@
+/*
+ *   SnowDemo: Create an attractive Christmas hat using an 8x32 matrix,
+ *             FastLED and LEDMatrix libraries.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * ESP8266 has too much flicker with Interrupts enabled
